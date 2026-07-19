7c2007fa0a803c35d5947a94bd9ae702b18eccd7
savitri <savitrigamit123456@gmail.com>
2026-07-19 22:53:10 +0530

    Update Nodemailer SMTP configuration

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,7 +6,9 @@ const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
